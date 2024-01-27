bddf5a22db8b4f425ae0f4223c962de2229127d4
Shashank P <shashankp2832@gmail.com>
2024-01-27 21:47:58 +0530

    Updated lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod generators;
 
 pub use generators::get_max_primes;
 pub use primality::{standard, fermat, miller_rabin, lucas_lehmer_test};
-pub use operations::pow;
+pub use operations::{gcd, pow_mod, pow, utils};
